8207ba70d512c74f567ee9524762860bece0ab29
Redokai <petroniobmesquita@gmail.com>
2020-02-07 00:48:49 -0300

    Add project files.

---
+++ DocImage/Program.cs
@@ -0,0 +1,22 @@
+﻿using OfficeExtension;
+
+namespace DocImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TABLE_TITLE = "BreakDown";
+            int COLUMN_INDEX = 0;
+            string IMAGE_PATH = @"C:\Users\Red\source\repos\DocImage\DocImage\gaivota.jpg";
+            string DOCUMENT_TEMPLATE_PATH = @"C:\Users\Red\source\repos\DocImage\DocImage\template.docx";
+            string DOCUMENT_OUTPUT_PATH = @"C:\Users\Red\source\repos\DocImage\DocImage\form.docx";
+
+            WordDocument DocClass = new WordDocument();
+            DocClass.OpenFile(DOCUMENT_TEMPLATE_PATH);
+            DocClass.AppendImageOnTableColumn(IMAGE_PATH, TABLE_TITLE, COLUMN_INDEX);
+            DocClass.SaveDocAs(DOCUMENT_OUTPUT_PATH);
+
+        }
+    }
+}
---
+++ OfficeExtension/Word.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Microsoft.Office.Interop.Word;
+
+namespace OfficeExtension
+{
+    public class WordDocument
+    {
+        private Document _doc;
+
+        public WordDocument()
+        {
+            this._doc = null;
+        }
+
+        public Document CreateFile()
+        {
+            Application app = new Application();
+            this._doc = app.Documents.Add();
+            return _doc;
+        }
+
+        public Document OpenFile(string filePath)
+        {
+            Application app = new Application();
+            this._doc = app.Documents.Open(filePath, Visible: false, ReadOnly: true);
+            return _doc;
+        }
+
+        public void SaveDocAs(string filepath)
+        {
+            this._doc.SaveAs2(filepath);
+        }
+
+
+        public void AppendImageOnTableColumn(string imagePath, string tableTitle, int columnIndex)
+        {
+            Table table = _FindTable(tableTitle);
+            Range insertRange = _FindTableEmptyCellOnSpecificCollumn(table, columnIndex);
+            if (insertRange == null)
+            {
+
+            }
+            _InsertPictureInRange(imagePath, insertRange);
+        }
+
+        public void AppendTextOnTableColumn(string text, string tableTitle, int columnIndex)
+        {
+            Table table = _FindTable(tableTitle);
+            Range insertRange = _FindTableEmptyCellOnSpecificCollumn(table, columnIndex);
+            if (insertRange == null)
+            {
+                _AppendTableRow(table);
+            }
+            insertRange = _FindTableEmptyCellOnSpecificCollumn(table, columnIndex);
+            _InsertTextInRange(text, insertRange);
+        }
+
+        private Table _FindTable(string tableTitle)
+        {
+            foreach (Table table in this._doc.Tables)
+            {
+                if (table.Title == tableTitle)
+                    return table;
+            }
+            return null;
+        }
+
+        private Range _FindTableEmptyCellOnSpecificCollumn(Table table, int columnIndex)
+        {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Cell cell = table.Cell(i, columnIndex);
+                if (cell.Range.Text != null && cell.Range.InlineShapes.Count == 0)
+                {
+                    return cell.Range;
+                }
+            }
+            return null;
+        }
+
+        private void _AppendTableRow(Table table)
+        {
+            table.Rows.Add();
+        }
+
+        private void _InsertPictureInRange(string path, Range range)
+        {
+            range.InlineShapes.AddPicture(path);
+        }
+        private void _InsertTextInRange(string text, Range range)
+        {
+            range.Text = text;
+        }
+    }
+
+
+}
